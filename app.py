1ffa458b00a2e19e96a430b4803e6aee8e8ce176
dj <david1a@ai-pros.com>
2020-01-23 15:19:59 +0800

    minor code revisions

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import sys
 import os
-# import glob
-# import re
-# import numpy as np
-
-# Keras
-# from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-# from keras.models import load_model
-# from keras.preprocessing import image
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template, jsonify
@@ -22,26 +14,22 @@ file_path = ""
 if not os.path.exists('uploads'):
     os.makedirs('uploads')
 
-
 @app.route('/', methods=['GET'])
 def index():
     # Main page
     return render_template('index.html')
 
-@app.route('/clone', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
     global file_path
-    print("in upload()")
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
-
 
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
-        print(file_path)
         f.save(file_path)
 
     return file_path
@@ -52,9 +40,8 @@ def get_script():
     if request.method == 'POST':
         # Get the transcript from post request
         script = request.get_json()
-        print("FILE PATH")
-        print(file_path)
 
+        # Start the cloning process then synthesize speech using the script provided
         output_path = Voice_Cloner.sythesize_voice(file_path, script)
         response = jsonify({'audio_id': output_path})
 
@@ -62,19 +49,13 @@ def get_script():
 
 @app.route('/record', methods=['POST'])
 def save_audio():
-    # f = open('./file.wav', 'wb')
-    # f.write(request.files['file'])
-    # f.close()
     global file_path
-    print("in save_audio()")
     f = request.files['file']
-
 
     # Save the file to ./uploads
     basepath = os.path.dirname(__file__)
     file_path = os.path.join(
         basepath, 'uploads', secure_filename(f.filename))
-    print(file_path)
     f.save(file_path)
 
     return 'Audio saved'
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
 
+  // Init
+  $('.filename-section').hide();
+  $('.clone').hide();
+  $('.input-form').hide();
+  $('.loader').hide();
+  // $('#result').hide();
+
   let log = console.log.bind(console),
   id = val => document.getElementById(val),
   ul = id('ul'),
-  // gUMbtn = id('gUMbtn'),
+  recordVoiceBtn = id('recordVoice'),
   start = id('start'),
   stop = id('stop'),
   stream,
@@ -12,44 +19,27 @@ $(document).ready(function () {
   chunks,
   media;
 
-  gUMbtn.onclick = e => {
-    // let mv = id('mediaVideo'),
-    //     mediaOptions = {
-    //       video: {
-    //         tag: 'video',
-    //         type: 'video/webm',
-    //         ext: '.mp4',
-    //         gUM: {video: true, audio: true}
-    //       },
-    //       audio: {
-    //         tag: 'audio',
-    //         type: 'audio/ogg',
-    //         ext: '.ogg',
-    //         gUM: {audio: true}
-    //       }
-    //     };
-    // media = mv.checked ? mediaOptions.video : mediaOptions.audio;
+  recordVoiceBtn.onclick = e => {
     navigator.mediaDevices.getUserMedia({ audio: true}).then(_stream => {
       stream = _stream;
-      id('gUMArea').style.display = 'none';
+      id('recordArea').style.display = 'none';
       id('btns').style.display = 'inherit';
       start.removeAttribute('disabled');
       recorder = new MediaRecorder(stream);
       recorder.ondataavailable = e => {
         chunks.push(e.data);
-        // const blobDataInWavFormat: Blob = new Blob([blobDataInWebaFormat], { type : 'audio/wav; codecs=0' });
-        if(recorder.state == 'inactive')  sendRawAudio(); //makeLink()
+        if(recorder.state == 'inactive')  recordAudio();
       };
       log('got media successfully');
     }).catch(log);
   }
+
   start.onclick = e => {
     start.disabled = true;
     stop.removeAttribute('disabled');
     chunks=[];
     recorder.start();
   }
-
 
   stop.onclick = e => {
     stop.disabled = true;
@@ -59,61 +49,18 @@ $(document).ready(function () {
     $('.input-form').show();
     $('.clone').show();
     $('#btn-clone').show();
-    //
-    // recorded_src
-    // audio.attr("src", sourceUrl);
-    // // Play wav URL
-    //   audio[0].pause();
-    //   audio[0].load(); //suspends and restores all audio element
   }
 
 
-  function makeLink(){
-    let blob = new Blob(chunks, {type: 'audio/ogg' })
-      , url = URL.createObjectURL(blob)
-      , li = document.createElement('li')
-      , mt = document.createElement('audio')
-      , hf = document.createElement('a')
-    ;
-    // link = document.createElement("a");
-    // link.href = url;
-    // link.download = "audiofile.ogg"
-    console.log(url)
-    mt.controls = true;
-    mt.src = url;
-    hf.href = url;
-    hf.download = `${counter++}${'.ogg'}`;
-    hf.innerHTML = `donwload ${hf.download}`;
-    // hf.click()
-    li.appendChild(mt);
-    li.appendChild(hf);
-    ul.appendChild(li);
-    console.log(url);
-    // save as wav or mp3 using ffmpeg https://medium.com/jeremy-gottfrieds-tech-blog/javascript-tutorial-record-audio-and-encode-it-to-mp3-2eedcd466e78
-    // var ffmpeg = require('ffmpeg');
-    // try{
-    //   var process = new ffmpeg(url);
-    //   console.log("ffmpeg");
-    // }
-    // catch(e){
-    //   console.log(e.code);
-    //   console.log(e.msg);
-    // }
-  }
-
-  function sendRawAudio(){
+  function recordAudio(){
     blob = new Blob(chunks, {type: 'audio/wav' })
     var form_data = new FormData();
     form_data.append('file', blob, 'recorded.wav');
-    console.log("sendRawAudioaudio");
     url = URL.createObjectURL(blob);
 
     $("#recorded_audio_player").controls = true;
-    $("#recorded_audio_player").attr("src", url);//.src = url;
+    $("#recorded_audio_player").attr("src", url);
     $("#recorded_audio_player").show();
-    // recordedAudioPlayer = document.createElement('audio');
-    // recordedAudioPlayer.controls = true;
-    // recordedAudioPlayer.src = url;
     $.ajax({
           type: 'POST',
           url: '/record',
@@ -128,12 +75,14 @@ $(document).ready(function () {
       });
 
   }
-    // Init
-    $('.filename-section').hide();
-    $('.clone').hide();
-    $('.input-form').hide();
-    $('.loader').hide();
-    $('#result').hide();
+
+  $("#audioUpload").change(function () {
+      $('.filename-section').show();
+      $('.input-form').show();
+      // $('#result').text('');
+      // $('#result').hide();
+      readURL(this);
+  });
 
     // Upload Preview
     function readURL(input) {
@@ -141,12 +90,11 @@ $(document).ready(function () {
             var reader = new FileReader();
             var filename = input.files[0].name;
             reader.onload = function (e) {
-                $('#filenamePreview').css('background-image', 'url(' + e.target.result + ')');
+                // $('#filenamePreview').css('background-image', 'url(' + e.target.result + ')');
                 $('#filenamePreview').hide();
                 $('#filenamePreview').text(filename);
                 $('#filenamePreview').fadeIn(650);
                 $('.clone').show();
-                $('#btn-clone').show();
                 upload_file();
             }
 
@@ -154,20 +102,15 @@ $(document).ready(function () {
         }
     }
 
-    $("#audioUpload").change(function () {
-        $('.filename-section').show();
-        $('.input-form').show();
-        $('#btn-predict').show();
-        $('#result').text('');
-        $('#result').hide();
-        // audio.show()
-        readURL(this);
-    });
+    // $("#audioUpload").change(function () {
+    //     $('.filename-section').show();
+    //     $('.input-form').show();
+    //     $('#btn-predict').show();
+    //     $('#result').text('');
+    //     $('#result').hide();
+    //     readURL(this);
+    // });
 
-    $("#audioRecorder").change(function () {
-        console.log("test");
-        recordAudio()
-    });
 
     function upload_file(){
       var form_data = new FormData($('#upload-file')[0]);
@@ -175,7 +118,7 @@ $(document).ready(function () {
 
       $.ajax({
             type: 'POST',
-            url: '/clone',
+            url: '/upload',
             data: form_data,
             contentType: false,
             cache: false,
@@ -187,51 +130,10 @@ $(document).ready(function () {
         });
     }
 
-    function recordAudio(){
-      player = document.getElementById('audioRecorder')
-      console.log("test");
-      // navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-      // .then(handleSuccess);
-
-      const handleSuccess = function(stream) {
-        console.log("success");
-        const options = {mimeType: 'audio/webm'};
-        const recordedChunks = [];
-        const mediaRecorder = new MediaRecorder(stream, options);
-
-        mediaRecorder.addEventListener('dataavailable', function(e) {
-          console.log("mediaRecorder");
-          if (e.data.size > 0) {
-            recordedChunks.push(e.data);
-          }
-
-          console.log("shouldStop")
-          if(shouldStop === true && stopped === false) {
-            mediaRecorder.stop();
-            stopped = true;
-          }
-        });
-
-        mediaRecorder.addEventListener('stop', function() {
-          downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
-          downloadLink.download = 'acetest.wav';
-          console.log("Saved")
-        });
-
-        mediaRecorder.start();
-
-      };
-
-      navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-      .then(handleSuccess);
-    }
 
     function get_script(){
-      // var form_script_data = new FormData($('#script')[0]);
       var form_script_data = document.getElementById('input-text').value;
-      // console.log("Script");
       console.log(form_script_data);
-
 
         $.ajax({
               type: 'POST',
@@ -243,6 +145,7 @@ $(document).ready(function () {
               processData: false,
               async: true,
               success: function (data) {
+                  $('.loader').hide();
                   sourceUrl = data['audio_id']
                   play_audio_file(audio=$("#audio_player"), wav_url=sourceUrl)
                   console.log('Success!');
@@ -259,16 +162,13 @@ $(document).ready(function () {
       	// Play wav URL
           audio[0].pause();
           audio[0].load(); //suspends and restores all audio element
-        //   audio[0].oncanplaythrough = audio[0].play();
       }
 
 
-    // Predict
+    // Clone voice and synthesize speech
     $('#btn-clone').click(function () {
-        // var form_data = new FormData($('#upload-file')[0]);
-        // var form_transcript_data = new FormData($('#script')[0]);
-        // upload_file();
         get_script();
+        $('.loader').show();
     });
 
 });
